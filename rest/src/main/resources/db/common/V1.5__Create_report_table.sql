d1ecde46fb63812cd5ea0caaaea5681d0ac8c623
Alexis-Dia <alexeydruzik@gmail.com>
2022-05-21 21:39:48 +0300

    Fixed task migration.

--- rest/src/main/resources/db/common/V1.5__Create_report_table.sql
+++ rest/src/main/resources/db/common/V1.5__Create_report_table.sql
@@ -8,18 +8,18 @@ CREATE TABLE IF NOT EXISTS REPORT (
     FOREIGN KEY (task_id)               REFERENCES task
 );
 
-INSERT INTO PROJECT(id, name, comment, create_date, hours, priority, budget, status) VALUES(1, 'Medical project', 'Created by Joe Dow', '2021-12-17 16:57:31.886321', 1000.0, 10, 50000.0, 'NEW');
-INSERT INTO PROJECT(id, name, comment, create_date, hours, priority, budget, status) VALUES(2, 'Car auction project', 'Created by Joe Dow', '2021-12-17 16:57:31.886321', 1000.0, 10, 50000.0, 'NEW');
-INSERT INTO PROJECT(id, name, comment, create_date, hours, priority, budget, status) VALUES(3, 'House renting project', 'Created by Joe Dow', '2021-12-17 16:57:31.886321', 1000.0, 10, 50000.0, 'NEW');
+INSERT INTO PROJECT(id, name, comment, create_date, hours, priority, budget, status) VALUES(1, 'Medical project', 'Created by admin', '2021-12-17 16:57:31.886321', 1000.0, 10, 50000.0, 'NEW');
+INSERT INTO PROJECT(id, name, comment, create_date, hours, priority, budget, status) VALUES(2, 'Car auction project', 'Created by admin', '2021-12-18 16:57:31.886321', 1000.0, 10, 50000.0, 'NEW');
+INSERT INTO PROJECT(id, name, comment, create_date, hours, priority, budget, status) VALUES(3, 'House renting project', 'Created by admin', '2021-12-19 16:57:31.886321', 1000.0, 10, 50000.0, 'NEW');
 
-INSERT INTO TASK(id, name, comment, create_date, project_id, status) VALUES(1, 'Task 1', 'Created by Joe Dow', '2021-12-17 17:57:31.886321', 1, 'NEW');
-INSERT INTO TASK(id, name, comment, create_date, project_id, status) VALUES(2, 'Task 2', 'Created by Joe Dow', '2021-12-17 17:57:31.886321', 1, 'NEW');
-INSERT INTO TASK(id, name, comment, create_date, project_id, status) VALUES(3, 'Task 2', 'Created by Joe Dow', '2021-12-17 18:57:31.886321', 1, 'IN_PROGRESS');
-INSERT INTO TASK(id, name, comment, create_date, project_id, status) VALUES(4, 'Task 3', 'Created by Joe Dow', '2021-12-17 19:57:31.886321', 1, 'ON_HOLD');
-INSERT INTO TASK(id, name, comment, create_date, project_id, status) VALUES(5, 'Task 1', 'Created by Joe Dow', '2021-12-17 19:57:31.886321', 1, 'FINISHED');
+INSERT INTO TASK(id, name, comment, create_date, project_id, status) VALUES(1, 'Creating user-table at the database', 'Created by admin', '2021-12-17 17:57:31.886321', 1, 'NEW');
+INSERT INTO TASK(id, name, comment, create_date, project_id, status) VALUES(2, 'Creating name-column at the user-table', 'Created by admin', '2021-12-18 17:57:31.886321', 1, 'NEW');
+INSERT INTO TASK(id, name, comment, create_date, project_id, status) VALUES(3, 'Creating rest-api for new users', 'Created by admin', '2021-12-19 18:57:31.886321', 1, 'IN_PROGRESS');
+INSERT INTO TASK(id, name, comment, create_date, project_id, status) VALUES(4, 'Change color of header', 'Created by admin', '2021-12-20 19:57:31.886321', 1, 'ON_HOLD');
+INSERT INTO TASK(id, name, comment, create_date, project_id, status) VALUES(5, 'Refactoring of code', 'Created by admin', '2021-12-21 19:57:31.886321', 1, 'FINISHED');
 
 INSERT INTO PROJECT_TO_USER(id, project_id, user_id) VALUES(1, 1, 1);
 INSERT INTO PROJECT_TO_USER(id, project_id, user_id) VALUES(2, 1, 2);
 
-INSERT INTO REPORT(id, name, comment, create_date, task_id) VALUES(1, 'Report 1', 'Created by Joe B', '2021-12-17 17:57:31.886321', 3);
-INSERT INTO REPORT(id, name, comment, create_date, task_id) VALUES(2, 'Report 2', 'Created by Joe B', '2021-12-17 18:57:31.886321', 3);
+INSERT INTO REPORT(id, name, comment, create_date, task_id) VALUES(1, 'Created repository-layer', 'Created by Ivan Ivanov', '2021-12-17 17:57:31.886321', 3);
+INSERT INTO REPORT(id, name, comment, create_date, task_id) VALUES(2, 'Created service-layer', 'Created by Ivan Ivanov', '2021-12-17 18:57:31.886321', 3);
